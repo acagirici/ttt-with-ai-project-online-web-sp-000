32911fe87f18b8df1fcaf2764a7c8306f8c313e0
Arif Cagirici <acagirici23@gmail.com>
2020-07-09 06:20:19 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,24 @@ module Computer
   Computer < Player
   
   def move
-    board.cells()
+    board.cells[0]
+    "1"
+    board.cells[1]
+    "2"
+    board.cells[2]
+    "3"
+    board.cells[3]
+    "4"
+    board.cells[4]
+    "5"
+    board.cells[5]
+    "6"
+    board.cells[6]
+    "7"
+    board.cells[7]
+    "8"
+    board.cells[8]
+    "9"
   end
   
 end
